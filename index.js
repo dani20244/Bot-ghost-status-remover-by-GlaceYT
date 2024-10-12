88bb5a93c951a5ddef3d5451af1ceaba96b6813b
dani20244 <dksfdsfgdfsgsg@gmail.com>
2024-10-12 17:15:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["My owner+ dev: dana2 👩‍💻", "My server: vibe & chill 😃"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
